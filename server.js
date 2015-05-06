50890e71051ecff56180d44403d589b5d9c6b8ce
droidraja <srajasudhan@gmail.com>
2015-05-06 21:16:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // get port from environment settings for deployment on Heroku
-var EXPRESS_PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000;
+var EXPRESS_PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 80;
 var EXPRESS_IPADDR = process.env.OPENSHIFT_NODEJS_IP || process.env.IPADDR || '127.0.0.1';
 var EXPRESS_ROOT = __dirname;
 
